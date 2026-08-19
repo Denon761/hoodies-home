3dcb64235da1dd33e6deb27006ddb3018bc7068b
Kilo <kilo@kilo.ai>
2026-08-20 01:43:13 +0500

    feat: add Microsoft Clarity tracking script to enhance user analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { Montserrat, Archivo_Black } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
@@ -53,6 +54,19 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="preconnect" href="https://js.stripe.com" />
         <link rel="preconnect" href="https://api.stripe.com" />
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "y4zs1z3ku5");
+            `,
+          }}
+        />
       </head>
       <body className="min-h-full flex flex-col">
         <Providers>
